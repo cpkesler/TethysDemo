420a119c236203533cc449817dd0ddee7ff39ee3
Christian Kesler <christiankesler11@gmail.com>
2016-05-04 13:17:23 -0600

    first commit

---
+++ tethysapp/my_first_app/app.py
@@ -0,0 +1,38 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class MyFirstApp(TethysAppBase):
+    """
+    Tethys app class for my first app.
+    """
+
+    name = 'my first app'
+    index = 'my_first_app:home'
+    icon = 'my_first_app/images/icon.gif'
+    package = 'my_first_app'
+    root_url = 'my-first-app'
+    color = '#34495e'
+    description = 'This is a demo app.'
+    enable_feedback = False
+    feedback_emails = []
+
+        
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (UrlMap(name='home',
+                           url='my-first-app',
+                           controller='my_first_app.controllers.home'),
+                    UrlMap(name='info',
+                           url='info',
+                           controller='my_first_app.controllers.info'),
+                    UrlMap(name='map',
+                           url='map',
+                           controller='my_first_app.controllers.map'),
+
+        )
+
+        return url_maps
---
+++ tethysapp/my_first_app/controllers.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+@login_required()
+def home(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+
+    return render(request, 'my_first_app/home.html', context)
+
+def info(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+
+    return render(request, 'my_first_app/info.html', context)
+
+def map(request):
+    """
+    Controller for the app home page.
+    """
+    context = {}
+
+    return render(request, 'my_first_app/map.html', context)
